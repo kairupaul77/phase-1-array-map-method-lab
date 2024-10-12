bde17b7a15141f5960f9c53e9d7fad003bdd14d0
paul kairu <paulkairukamau@gmail.com>
2024-10-12 23:27:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+// const tutorials = [
+  //   'what does the this keyword mean?',
+  //   'What is the Constructor OO pattern?',
+  //   'implementing Blockchain Web API',
+  //   'The Test Driven Development Workflow',
+  //   'What is NaN and how Can we Check for it',
+  //   'What is the difference between stopPropagation and preventDefault?',
+  //   'Immutable State and Pure Functions',
+  //   'what is the difference between == and ===?',
+  //   'what is the difference between event capturing and bubbling?',
+  //   'what is JSONP?'
+  // ];
+  
+  // const titleCased = () => {
+  //   return tutorials
+  // }
+  
+  const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  
+  const titleCased = () => {
+    return tutorials.map(tutorial => {
+      const splittedArr = tutorial.split(" ");
+      const capitalized = splittedArr.map(
+        (str) => str.charAt(0).toUpperCase() + str.slice(1)
+      );
+      return capitalized.join(" ");
+    });
+  };
+  
+  console.log(titleCased());
 
-const titleCased = () => {
-  return tutorials
-}
